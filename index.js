931410928d681cc1887ad77ccf3109c15ea4d882
leoiscoolxd <mrtopm16@gmail.com>
2024-09-24 16:58:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["Watching Sea World MM Services"];
-const statusTypes = [ 'dnd'];
+const statusTypes = [ 'dnd' ];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
